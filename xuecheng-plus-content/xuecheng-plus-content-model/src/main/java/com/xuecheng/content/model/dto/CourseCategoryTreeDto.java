743a8a2da4c56e572d2707725c8ea44804bc83ff
gwj <2256030729@qq.com>
2024-10-18 20:12:59 +0800

    第二章4完成

--- xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
@@ -6,10 +6,10 @@ import lombok.Data;
 import java.util.List;
 
 /**
- * @author Mr.M
+ * @author gwj
  * @version 1.0
  * @description TODO
- * @date 2023/2/12 11:51
+ * @date 2024/10/18 11:51
  */
 @Data
 public class CourseCategoryTreeDto extends CourseCategory implements java.io.Serializable {
