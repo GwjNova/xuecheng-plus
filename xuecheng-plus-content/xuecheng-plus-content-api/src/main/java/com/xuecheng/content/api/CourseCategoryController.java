29af8ba3f1a309daeaf84be4d71eb400dc152fbd
gwj <2256030729@qq.com>
2024-10-18 20:16:38 +0800

    第二章4完成

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -0,0 +1,29 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseCategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+/**
+ * <p>
+ * 数据字典 前端控制器
+ * </p>
+ *
+ * @author gwj
+ */
+@RestController
+@Slf4j
+public class CourseCategoryController {
+
+    @Autowired
+    private CourseCategoryService courseCategoryService;
+
+    @GetMapping("/course-category/tree-nodes")
+    public List<CourseCategoryTreeDto> queryTreeNodes() {
+        return courseCategoryService.queryTreeNodes("1");
+    }
+}
